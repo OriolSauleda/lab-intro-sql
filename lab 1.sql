63c5a5b38194c54a378441b5c6fdb177c42ce781
Uri Sauleda <osauledacasas@gmail.com>
2021-02-02 00:29:24 +0100

    adding lab 1 to the repo

---
+++ lab 1.sql
@@ -0,0 +1,11 @@
+select * from actor;
+-- wfgerg
+select * from customer;
+select city from city;
+-- weergv
+select distinct name as language from language;
+select count(staff_id) from staff;
+select count(store_id) from store;
+select * from staff;
+select * from store;
+select count(distinct left(rental_date, 10)) from rental;
